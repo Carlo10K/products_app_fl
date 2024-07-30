05c45aba1017f2af0ff5cf0a2e9735a36d84c9ff
Carlo10K <carlosortegadiaz50@gmail.com>
2024-07-30 11:22:42 -0600

    agregado los valores al form de edicion

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:products_app/providers/product_form_provider.dart';
 import 'package:products_app/services/services.dart';
 import 'package:products_app/ui/input_decorations.dart';
 import 'package:products_app/widgets/widgets.dart';
@@ -11,8 +13,25 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productService = Provider.of<ProductsService>(context);
 
+    return ChangeNotifierProvider(
+        create: (_) => ProductFormProvider(productService.selectedProduct),
+        child: _ProductScreenBody(productService: productService));
+  }
+}
+
+class _ProductScreenBody extends StatelessWidget {
+  const _ProductScreenBody({
+    super.key,
+    required this.productService,
+  });
+
+  final ProductsService productService;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
         child: Column(
           children: [
             Stack(
@@ -44,7 +63,7 @@ class ProductScreen extends StatelessWidget {
                         )))
               ],
             ),
-            _productForm(),
+            _ProductForm(),
             const SizedBox(
               height: 100,
             )
@@ -65,9 +84,12 @@ class ProductScreen extends StatelessWidget {
   }
 }
 
-class _productForm extends StatelessWidget {
+class _ProductForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productForm = Provider.of<ProductFormProvider>(context);
+    final product = productForm.product;
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Container(
@@ -81,6 +103,14 @@ class _productForm extends StatelessWidget {
                 height: 10,
               ),
               TextFormField(
+                initialValue: product.name,
+                onChanged: (value) => product.name = value,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'El nombre es obligatorio';
+                  }
+                  return null;
+                },
                 decoration: InputDecorations.authInputDecoration(
                     hintText: 'Nombre del producto', labelText: 'Nombre:'),
               ),
@@ -88,6 +118,24 @@ class _productForm extends StatelessWidget {
                 height: 30,
               ),
               TextFormField(
+                initialValue: '${product.price}',
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(
+                      RegExp(r'^(\d+)?\.?\d{0,2}'))
+                ],
+                onChanged: (value) {
+                  if (double.tryParse(value) == null) {
+                    product.price = 0;
+                  } else {
+                    product.price = double.parse(value);
+                  }
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'El precio es obligatorio';
+                  }
+                  return null;
+                },
                 keyboardType: TextInputType.number,
                 decoration: InputDecorations.authInputDecoration(
                     hintText: '\$150', labelText: 'Precio:'),
@@ -96,10 +144,10 @@ class _productForm extends StatelessWidget {
                 height: 30,
               ),
               SwitchListTile.adaptive(
-                  value: true,
-                  title: Text('Disponible'),
+                  value: product.available,
+                  title: const Text('Disponible'),
                   activeColor: Colors.indigo,
-                  onChanged: (value) {}),
+                  onChanged: productForm.updateAvailability),
               const SizedBox(
                 height: 30,
               )
